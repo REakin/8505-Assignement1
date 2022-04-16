a840e7a90f0d36e020ac5bdcdfab5a110264f75a
Ryan Eakin <Eakinryan@hotmail.com>
2022-04-16 12:00:37 -0700

    updated client.py

--- Client.py
+++ Client.py
@@ -1,9 +1,11 @@
 from itertools import count
+from struct import pack
 import scapy
 import os
 import sys
 import threading
 from scapy.all import *
+from binascii import hexlify
 
 def ceaser(text, shift):
     cyphertext = ''
@@ -19,14 +21,19 @@ def ceaser(text, shift):
 
 def packet_callback(packet):
     if packet[UDP].dport == 20000:
-        print("[+] Received encrypted message: " + packet[Raw].load)
-        print("[+] Decrypted message: " + ceaser(packet[Raw].load, -shift))
+        print("[+] Received encrypted message: " + (packet[Raw].load).decode())
+        #extract the checksum
+        checksum = chr(packet[UDP].chksum)
+        print("[+] Checksum: " + str(checksum))
+        #extract the shift
+        shift = (packet[Raw].load).decode()
+        print("[+] decoded message: " + ceaser(checksum, -int(shift)))
 
 def main():
     #sniff packets
     print("[+] Sniffing for encrypted messages...")
     #sniff packets on loopback interface
-    sniff(iface="Ethernet 2", filter="udp", prn=lambda x: packet_callback(x), count=0)
+    sniff(iface="Wi-Fi", filter="udp", prn=lambda x: packet_callback(x), count=0)
 
 
 if __name__ == '__main__':
